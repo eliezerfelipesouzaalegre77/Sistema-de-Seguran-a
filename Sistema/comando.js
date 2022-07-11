56e605943e6d8352248cfbc93124a285497702dc
Eliezer felipe souza alegre <109081814+eliezerfelipesouzaalegre77@users.noreply.github.com>
2022-07-11 09:59:23 -0300

    Commit 1

---
+++ Sistema/comando.js
@@ -0,0 +1,11 @@
+function clicou1(){
+    const Audio = document.querySelector('audio')
+    Audio.currentTime = 0
+    Audio.play()    
+}
+
+function load001(){
+    const Audio = document.querySelector('audio')
+    Audio.currentTime = 0
+    Audio.play()
+}
